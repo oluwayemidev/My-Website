ccdee13f3ac12f1ee303a8b7de1cf0af233ed136
Oluwayemi <lordryefox@gmail.com>
2022-07-20 06:59:08 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname + 'public'))); // default
 
 app.get('/api/customers', (req, res) => {
     const customers = [
@@ -13,5 +16,5 @@ app.get('/api/customers', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server started`);
+    console.log(`Server started on ${port}`);
 });
