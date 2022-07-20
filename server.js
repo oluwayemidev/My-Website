82d80e859d2561b1bd69915d821de9b8ff04f768
Oluwayemi <lordryefox@gmail.com>
2022-07-20 05:52:07 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/api/customers', (req, res) => {
     const customers = [
